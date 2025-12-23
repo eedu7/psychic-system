bd90d26fb4525835218cc21fc30275637ec178bb
eedu7 <mueedahmad067@gmail.com>
2025-12-23 19:21:57 +0500

    I'm Batman

---
+++ src/constants/students.ts
@@ -0,0 +1,59 @@
+type Student = {
+    name: string;
+    rollNumber: string;
+    goal: string;
+    section: string;
+    department: string;
+};
+
+export const students: Student[] = [
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+    {
+        department: "Software Engineering",
+        name: "Mueed Ahmad",
+        goal: "Create a new ERA",
+        rollNumber: "S22BSEEN1M01079",
+        section: "M2",
+    },
+];
